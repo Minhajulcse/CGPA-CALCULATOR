a789fe404c2a79226e9d9c6ae28ac4adb61327dd
Minhajul Islam <137941240+Minhajulcse@users.noreply.github.com>
2026-04-30 11:15:28 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'diu-cgpa-cache-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event: Save files to offline cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
